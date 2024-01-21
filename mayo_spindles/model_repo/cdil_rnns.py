62b54e8b6c31cfa26d8de4a8f893887c48d6c14b
CaptainTrojan <hyas@seznam.cz>
2024-01-21 18:19:30 +0100

    Add CDIL RNN models to model repository

---
+++ mayo_spindles/model_repo/cdil_rnns.py
@@ -0,0 +1,32 @@
+from copy import deepcopy
+from mayo_spindles.model_repo.cdil import CDILModel
+from mayo_spindles.model_repo.basic_models import RNNModel, GRUModel, LSTMModel
+from mayo_spindles.model_repo.base import BaseModel
+
+class CDILRNNBaseModel(BaseModel):
+    def __init__(self, config, cell_cls):
+        super().__init__(config)
+        self.cdil = CDILModel(config)
+        config = deepcopy(config)
+        config['num_layers'] = 1
+        config['c_in'] = config['c_out']
+        config['hidden_size'] = config['c_out']
+        self.rnn = cell_cls(config)
+    
+    def forward(self, x):
+        x = self.cdil(x)
+        x = self.rnn(x)
+        return x
+    
+
+class CDILRNNModel(CDILRNNBaseModel):
+    def __init__(self, config):
+        super().__init__(config, RNNModel)
+
+class CDILGRUModel(CDILRNNBaseModel):
+    def __init__(self, config):
+        super().__init__(config, GRUModel)
+
+class CDILLSTMModel(CDILRNNBaseModel):
+    def __init__(self, config):
+        super().__init__(config, LSTMModel)
