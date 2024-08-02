aa87881e46a5d140956ed3ccf7b446ddba353c20
Captain Trojan <hyas@seznam.cz>
2024-08-02 14:41:27 +0200

    refactor: Update `mayo_spindles/dataset_stats.py` to improve code organization and remove unnecessary imports

--- mayo_spindles/dataset_stats.py
+++ mayo_spindles/dataset_stats.py
@@ -1,49 +1,54 @@
-import time
 from dataloader import HDF5Dataset
 from tqdm import tqdm
 from plotly.subplots import make_subplots
-
 import plotly.graph_objects as go
+import cProfile
+import pstats
 
-# Start the timer
-start_time = time.time()
+def main():
+    dataset = HDF5Dataset('hdf5_data', split='train', augmentations_size=2000)
+    spindle_lengths = []
+    spindle_counts = []
+    for (X, y) in tqdm(dataset, desc='Iterating over dataset', total=len(dataset)):
+        dets = y['detection']
+        dets = dets[dets[:, 0] == 1]
+        spindle_counts.append(len(dets))
+        spindle_lengths.extend(dets[:, 2].detach().cpu().numpy())
 
-dataset = HDF5Dataset('hdf5_data', split='train', augmentations_size=2000)
-spindle_lengths = []
-spindle_counts = []
-for (X, y) in tqdm(dataset, desc='Iterating over dataset', total=len(dataset)):
-    dets = y['detection']
-    dets = dets[dets[:, 0] == 1]
-    spindle_counts.append(len(dets))
-    spindle_lengths.extend(dets[:, 2])
+    # Create subplots
+    fig = make_subplots(rows=2, cols=1, subplot_titles=('Spindle Counts', 'Spindle Lengths'))
 
-# Create subplots
-fig = make_subplots(rows=2, cols=1, subplot_titles=('Spindle Counts', 'Spindle Lengths'))
+    # Add spindle counts histogram
+    fig.add_trace(go.Histogram(
+        x=spindle_counts,
+        name='Spindle Counts',
+        opacity=0.75
+    ), row=1, col=1)
 
-# Add spindle counts histogram
-fig.add_trace(go.Histogram(
-    x=spindle_counts,
-    name='Spindle Counts',
-    opacity=0.75
-), row=1, col=1)
+    # Add spindle lengths histogram
+    fig.add_trace(go.Histogram(
+        x=spindle_lengths,
+        name='Spindle Lengths',
+        opacity=0.75
+    ), row=2, col=1)
 
-# Add spindle lengths histogram
-fig.add_trace(go.Histogram(
-    x=spindle_lengths,
-    name='Spindle Lengths',
-    opacity=0.75
-), row=2, col=1)
+    # Update layout
+    fig.update_layout(
+        title='Spindle Counts and Lengths Histograms',
+        xaxis_title='Value',
+        yaxis_title='Count'
+    )
 
-# Update layout
-fig.update_layout(
-    title='Spindle Counts and Lengths Histograms',
-    xaxis_title='Value',
-    yaxis_title='Count'
-)
+    # Show the plot
+    fig.show()
 
-# Show the plot
-fig.show()
+if __name__ == "__main__":
+    # Profile the main function
+    profiler = cProfile.Profile()
+    profiler.enable()
+    main()
+    profiler.disable()
 
-# Calculate the elapsed time
-elapsed_time = time.time() - start_time
-print(f"Elapsed time: {elapsed_time} seconds")
+    # Print profiling results
+    stats = pstats.Stats(profiler).sort_stats('tottime')
+    stats.print_stats(20).print_callers(20)
