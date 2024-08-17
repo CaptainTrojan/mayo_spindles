545467a5a761c1be30fcd9c2a5f8f9cc4354f412
Captain Trojan <hyas@seznam.cz>
2024-08-17 19:52:53 +0200

    refactor: Update 2_gridsearch.sh to support multiple repeats

--- 2_gridsearch.sh
+++ 2_gridsearch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if the number of repeats is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number_of_repeats>"
+  exit 1
+fi
+
+# Number of repeats
+repeats=$1
+
 # models=("mlp" "cnn" "rnn" "gru" "lstm" "cdil" "autoformer" "crossformer" "dlinear" "etsformer" "fedformer" "film" "fret")
 models=("cdil")
 share_bottleneck=("True" "False")
@@ -8,7 +17,9 @@ for model in "${models[@]}"
 do
     for shb in "${share_bottleneck[@]}"
     do
-        qsub -v "args=--epochs 1000 --model $model --share_bottleneck $shb" 1_run_instance.sh
+        for ((i=0; i<repeats; i++))
+        do
+            qsub -v "args=--epochs 1000 --patience 100 --model $model --share_bottleneck $shb" 1_run_instance.sh
+        done
     done
 done
-
