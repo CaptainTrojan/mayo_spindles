6875a86c9c2404dcc3d9a58d4492d44dc36060a8
captaintrojan@ct.cz <captaintrojan@ct.cz>
2024-08-18 04:11:53 +0200

    updated paths

--- 2_gridsearch.sh
+++ 2_gridsearch.sh
@@ -13,9 +13,9 @@ repeats=$2
 
 # Set data path and additional arguments based on the dataset
 if [ "$dataset" == "mayoieeg" ]; then
-  data_args="--data \$SCRATCHDIR/mayo_spindles/hdf5_data"
+  data_args="--data hdf5_data"
 elif [ "$dataset" == "dreams" ]; then
-  data_args="--data \$SCRATCHDIR/mayo_spindles/DREAMS_HDF5 --annotator_spec any"
+  data_args="--data DREAMS_HDF5 --annotator_spec any"
 else
   echo "Invalid dataset option. Choose either 'mayoieeg' or 'dreams'."
   exit 1
--- 5_smoke.sh
+++ 5_smoke.sh
@@ -12,9 +12,9 @@ dataset=$1
 
 # Set data path and additional arguments based on the dataset
 if [ "$dataset" == "mayoieeg" ]; then
-  data_args="--data \$SCRATCHDIR/mayo_spindles/hdf5_data"
+  data_args="--data hdf5_data"
 elif [ "$dataset" == "dreams" ]; then
-  data_args="--data \$SCRATCHDIR/mayo_spindles/DREAMS_HDF5 --annotator_spec any"
+  data_args="--data DREAMS_HDF5 --annotator_spec any"
 else
   echo "Invalid dataset option. Choose either 'mayoieeg' or 'dreams'."
   exit 1
