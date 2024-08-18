d8f04a765cec0c0821aa682acd27762af59534e8
Captain Trojan <hyas@seznam.cz>
2024-08-18 03:56:12 +0200

    refactor: Update 5_smoke.sh to handle different dataset specifications and improve dataset validation

--- 5_smoke.sh
+++ 5_smoke.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-qsub -v "args=--patience 5 --model cdil" 1_run_instance.sh
+# Check if the dataset is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <dataset>"
+  echo "Options for dataset: mayoieeg, dreams"
+  exit 1
+fi
 
+# Dataset
+dataset=$1
+
+# Set data path and additional arguments based on the dataset
+if [ "$dataset" == "mayoieeg" ]; then
+  data_args="--data \$SCRATCHDIR/mayo_spindles/hdf5_data"
+elif [ "$dataset" == "dreams" ]; then
+  data_args="--data \$SCRATCHDIR/mayo_spindles/DREAMS_HDF5 --annotator_spec any"
+else
+  echo "Invalid dataset option. Choose either 'mayoieeg' or 'dreams'."
+  exit 1
+fi
+
+qsub -v "args=$data_args --patience 5 --model cdil" 1_run_instance.sh
