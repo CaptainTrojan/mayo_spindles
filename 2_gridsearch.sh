798362e4172fa9849e631f686b4310a07e85cd42
Captain Trojan <hyas@seznam.cz>
2024-08-18 14:32:17 +0200

    update 2_gridsearch.sh to use optuna

--- 2_gridsearch.sh
+++ 2_gridsearch.sh
@@ -2,7 +2,7 @@
 
 # Check if the dataset and number of repeats are provided
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <dataset> <number_of_repeats>"
+  echo "Usage: $0 <dataset> <number_of_parallel_samplers>"
   echo "Options for dataset: mayoieeg, dreams"
   exit 1
 fi
@@ -21,29 +21,7 @@ else
   exit 1
 fi
 
-# models=("mlp" "cnn" "rnn" "gru" "lstm" "cdil" "autoformer" "crossformer" "dlinear" "etsformer" "fedformer" "film" "fret")
-models=("cdil")
-share_bottleneck=("True" "False")
-hidden_sizes=(32 64 128)
-conv_dropouts=(0.0 0.2 0.5)
-end_dropouts=(0.0 0.2 0.5)
-
-for model in "${models[@]}"
+for ((i=0; i<repeats; i++))
 do
-    for shb in "${share_bottleneck[@]}"
-    do
-        for hidden_size in "${hidden_sizes[@]}"
-        do
-            for conv_dropout in "${conv_dropouts[@]}"
-            do
-                for end_dropout in "${end_dropouts[@]}"
-                do
-                    for ((i=0; i<repeats; i++))
-                    do
-                        qsub -v "args=$data_args --epochs 1000 --patience 60 --model $model --share_bottleneck $shb --hidden_size $hidden_size --conv_dropout $conv_dropout --end_dropout $end_dropout" 1_run_instance.sh
-                    done
-                done
-            done
-        done
-    done
+    qsub -v "args=$data_args --epochs 1000 --patience 60 --model cdil" 1_run_instance.sh
 done
