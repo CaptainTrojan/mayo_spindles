0161cdf036841ed103911c271ffc6b5025e8630b
Captain Trojan <hyas@seznam.cz>
2024-08-18 03:43:19 +0200

    refactor: Update mayo_spindles/visualizer.py to include --split command line argument

--- mayo_spindles/visualizer.py
+++ mayo_spindles/visualizer.py
@@ -157,9 +157,10 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--data', type=str, default='hdf5_data', help='Path to HDF5 dataset')
     argparser.add_argument('--annotator_spec', type=str, default='', help='Annotator specification')
+    argparser.add_argument('--split', choices=['train', 'val', 'test'], default='train', help='Dataset split')
     args = argparser.parse_args()
     
-    dataset = HDF5Dataset(args.data, split='train', use_augmentations=True, annotator_spec=args.annotator_spec)
+    dataset = HDF5Dataset(args.data, split=args.split, use_augmentations=True, annotator_spec=args.annotator_spec)
     print(f"Loaded dataset with {len(dataset)} elements ")
     
     app = QApplication(sys.argv)
