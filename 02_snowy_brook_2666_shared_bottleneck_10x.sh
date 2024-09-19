7ff482e102292c7f1422fed8e11a85aaa30ec33f
Captain Trojan <hyas@seznam.cz>
2024-09-19 11:50:52 +0200

    Fixed error

---
+++ 02_snowy_brook_2666_shared_bottleneck_10x.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./3_custom.sh mayoieeg 10 "--model cdil --conv_dropout 0.452779725810959 --end_dropout 0.23822355799164377 --hidden_size 98 --mode shared_bottleneck --patience 60"
--- 02_vague_terrain_2714_shared_bottleneck_10x.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./3_custom.sh mayoieeg 10 "--model cdil --conv_dropout 0.486966943276408 --end_dropout 0.365823209304437 --hidden_size 67 --mode shared_bottleneck --patience 60"
